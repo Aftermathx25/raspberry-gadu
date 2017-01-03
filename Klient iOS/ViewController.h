e8f262ec262be165ccbf48c09aecf15bc630dcd5
Aftermath <aftermath@aftermath-pc.home>
2017-01-03 20:06:10 +0100

    Initial Commit

---
+++ Klient iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Klient iOS
+//
+//  Created by Aftermath on 03.01.2017.
+//  Copyright © 2017 Aftermath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
